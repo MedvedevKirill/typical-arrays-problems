1f105cf9da8d189e4496f66a4463c88fa77c4ff7
Kirill Medvedev <kiri_x@icloud.com>
2020-10-03 21:25:30 +0500

    feat: add min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if(array != null && array.length > 0){
+        let min = array[0];
+        array.forEach(element => {
+            if(element < min) min = element;
+        });
+        return min;
+    }
+    else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if(array != null && array.length > 0){
+        let max = array[0];
+        array.forEach(element => {
+            if(element > max) max = element;
+        });
+        return max;
+    }
+    else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if(array != null && array.length > 0){
+        let avg = 0;
+        array.forEach(element => {
+            avg += element;
+        });
+        return avg / array.length;
+    }
+    else return 0;
 }
